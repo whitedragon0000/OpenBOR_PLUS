a1ed9f561a17d69c1a5cf9b52b0f6845a94e7c72
White Dragon <000whitedragon000@gmail.com>
2019-07-16 21:32:43 +0200

    Fix Wii Menu after merge add mingit tool

--- engine/wii/menu.c
+++ engine/wii/menu.c
@@ -351,6 +351,7 @@ static int findPaks(void)
 	DIR* dp = NULL;
 	struct dirent* ds;
 	dp = opendir(paksDir);
+
 	if(dp != NULL)
    	{
    	    filelist = NULL;
@@ -358,18 +359,19 @@ static int findPaks(void)
 		{
 			if(packfile_supported(ds->d_name))
 			{
-				fileliststruct *copy = NULL;
-				if(filelist == NULL) filelist = malloc(sizeof(fileliststruct));
+				if(filelist == NULL) filelist = malloc(sizeof(struct fileliststruct));
 				else
 				{
-					copy = malloc(i * sizeof(fileliststruct));
-					memcpy(copy, filelist, i * sizeof(fileliststruct));
+				    filelist = (fileliststruct *)realloc(filelist, (i+1) * sizeof(struct fileliststruct));
+				    /*fileliststruct *copy = NULL;
+					copy = malloc((i + 1) * sizeof(struct fileliststruct));
+					memcpy(copy, filelist, (i + 1) * sizeof(struct fileliststruct));
 					free(filelist);
-					filelist = malloc((i + 1) * sizeof(fileliststruct));
-					memcpy(filelist, copy, i * sizeof(fileliststruct));
-					free(copy); copy = NULL;
+					filelist = malloc((i + 1) * sizeof(struct fileliststruct));
+					memcpy(filelist, copy, (i + 1) * sizeof(struct fileliststruct));
+					free(copy); copy = NULL;*/
 				}
-				memset(&filelist[i], 0, sizeof(fileliststruct));
+				memset(&filelist[i], 0, sizeof(struct fileliststruct));
 				strcpy(filelist[i].filename, ds->d_name);
 				i++;
 			}
