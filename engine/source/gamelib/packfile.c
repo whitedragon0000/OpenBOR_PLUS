04a6690f256610008500f27bcb7ebfe57a55f733
White Dragon <000whitedragon000@gmail.com>
2020-12-09 21:18:27 +0100

    rebase to official

--- engine/source/gamelib/packfile.c
+++ engine/source/gamelib/packfile.c
@@ -442,9 +442,9 @@ int openPackfile(const char *filename, const char *packfilename)
 #endif
 
     packfilepointer[h] = 0;
-
+	int per = 666;
     // Separate file present?
-    if((handle = open(filename, O_RDONLY | O_BINARY, 777)) != -1)
+    if((handle = open(filename, O_RDONLY | O_BINARY, per)) != -1)
     {
         if((packfilesize[h] = lseek(handle, 0, SEEK_END)) == -1)
         {
@@ -471,7 +471,7 @@ int openPackfile(const char *filename, const char *packfilename)
     fspath = casesearch(".", filename);
     if (fspath != NULL)
     {
-        if((handle = open(fspath, O_RDONLY | O_BINARY, 777)) != -1)
+        if((handle = open(fspath, O_RDONLY | O_BINARY, per)) != -1)
         {
             if((packfilesize[h] = lseek(handle, 0, SEEK_END)) == -1)
             {
@@ -501,7 +501,7 @@ int openPackfile(const char *filename, const char *packfilename)
 #endif
 
     // Try to open packfile
-    if((handle = open(packfilename, O_RDONLY | O_BINARY, 777)) == -1) //O_CREAT
+    if((handle = open(packfilename, O_RDONLY | O_BINARY, per)) == -1)
     {
 #ifdef VERBOSE
         printf ("perm err\n");
@@ -1255,8 +1255,8 @@ int pak_init()
     else
     {
 #endif
-
-        pakfd = open(packfile, O_RDONLY | O_BINARY, 777);
+	int per = 666;
+        pakfd = open(packfile, O_RDONLY | O_BINARY, per);
 
         if(pakfd < 0)
         {
@@ -1272,7 +1272,7 @@ int pak_init()
 
     // Is it a valid Packfile
     close(pakfd);
-    pakfd = open(packfile, O_RDONLY | O_BINARY, 777);
+    pakfd = open(packfile, O_RDONLY | O_BINARY, per);
 
     // Read magic dword ("PACK")
     if(read(pakfd, &magic, 4) != 4 || magic != SwapLSB32(PACKMAGIC))
